093876f77c1c401afb166a5db30eb95056486a38
hyojeongindeyo <ghdgywjd510@naver.com>
2023-07-13 19:20:33 +0900

    views

--- travelProject/place/views.py
+++ travelProject/place/views.py
@@ -1,2 +1,15 @@
 from django.shortcuts import render
-from .models import 
+from .models import
+from .forms import ReviewForm
+
+ef review_new(request):
+    if request.method == 'POST' :
+        form = ReviewForm(request.POST)
+        if form.is_valid() :
+            review = form.save(commit=False)
+            review.save()
+            return redirect('/', pk = review.pk)
+    else :
+        form = ReviewForm()
+
+    return render(request, 'yeogi/street-safety-comment.html', {'form' : form})
