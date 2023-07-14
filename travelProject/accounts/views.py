35f89f9d6860bbfb4ac81ab04bdf728a1646e785
soeun <so8439@naver.com>
2023-07-14 12:54:08 +0900

    가장 최근 커밋

--- travelProject/accounts/views.py
+++ travelProject/accounts/views.py
@@ -10,8 +10,22 @@ def join(request):
 			auth.login(request, new_user)
 			print('회원가입 성공')
 			return redirect('home')
-	else:
+		else:
+			return render(request, 'join.html')
+	else :
 		return render(request, 'join.html')
 
 def login(request):
-	return render(request, 'login.html')
+	if request.method == "POST" :
+		username = request.POST["userName"]
+		password = request.POST["userPassword"]
+		user = auth.authenticate(request, username=username, password=password)
+		
+		if user is not None :
+			auth.login(request, user)
+			print("로그인 성공")
+			return redirect('home')
+		else :
+			return render(request, 'bad_login.html')
+	else :
+		return render(request, 'login.html')
